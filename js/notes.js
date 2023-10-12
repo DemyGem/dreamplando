92081637be0c26d6cf27e86d4114abf5d63c0fa2
DemyGem <katita.gamings@gmail.com>
2023-10-12 16:42:10 -0300

    js

--- js/notes.js
+++ js/notes.js
@@ -1,2 +1,12 @@
 const notesContainer = document.querySelector(".notes-conteiner");
 const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let i = document.createElement("i");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    
+    notesContainer.appendChild(inputBox).appendChild(i);
+})
